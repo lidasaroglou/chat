a8247beeceb9ce73a5ee572e3b477ddebf7405ed
Kostas Tsopanakis <k.tsopanakis@dotbydot.gr>
2017-06-15 13:48:33 +0300

    Connection me server - Apostoli minimatos - Emfanisi minimatos ston Client

--- Client/ChatClient/ChatClient/MainWindow.xaml.cs
+++ Client/ChatClient/ChatClient/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.IO;
 
 namespace ChatClient
 {
@@ -20,9 +22,58 @@ namespace ChatClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        public event EventHandler<string> Message;
+        TcpClient tcpClient;
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                tcpClient = new TcpClient();
+                tcpClient.Connect("192.168.11.100", 5002);
+
+                Stream stm = tcpClient.GetStream();
+
+                String useraname = "giannis";
+                var writer = new StreamWriter(stm) { AutoFlush = true };
+
+                writer.WriteLine(useraname);
+
+
+                while (true)
+                {
+                    var reader = new StreamReader(stm);
+
+                    var line = await reader.ReadLineAsync();
+                    output.Text += line;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.Key != Key.Return) return;
+
+            var writer = new StreamWriter(tcpClient.GetStream()) { AutoFlush = true };
+            
+            writer.WriteLine(((TextBox)sender).Text);
+            
+            //Message?.Invoke(this, ((TextBox)sender).Text);
+            //((TextBox)sender).Text = "";
         }
     }
 }
